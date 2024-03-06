e8a7d5f4ce54fcafa5e9267dec381ea7398f6bb3
Walter G Davies - ASCCON <wgd@asccon.co>
2024-03-05 20:59:02 -0700

    #close 4-directory-recursion  #comment  * Directory recursion is being punted to a Version 2 release.   - Nonetheless there are some notable improvements herein....  * Function, structure, and variable declarations/definitions are much more consistent.  * No egregiously extraneous functions remaining.  * Other old/stub/commented code removed.  * Dprint()s trimmed down a bit.  * Introducing some half-clever compile time structure usage.

--- dstat.c
+++ dstat.c
@@ -50,6 +50,7 @@
 /**
  * Comparison strings for not descending into ourselves.
  */
+const char *DFS = "/";
 const char *CD = ".";
 const char *PD = "..";
 
@@ -57,12 +58,6 @@ const char *PD = "..";
  * CArgs library struct with options and documentation.
  */
 static struct cag_option options[] = {
-    {.identifier = 'r',
-     .access_letters = "r",
-     .access_name = "recursive",
-     .value_name = NULL,
-     .description = "Recurse down directories and include aggregated results."},
-
     {.identifier = 'v',
      .access_letters = "v",
      .access_name = "version",
@@ -85,8 +80,8 @@ static struct cag_option options[] = {
  */
 struct sel_opts_s opt = {
     // Default values for sel_opts{}.
-    .rec = false, .upd = false, .lin = false,
-    .qit = false, .out = false, .log = false,
+    .upd = false, .lin = false, .qit = false,
+    .out = false, .log = false,
     .outfile = "", .logfile = ""
 };
 
@@ -98,122 +93,157 @@ struct dir_ent_s de = {
     // Default values for dir_ent_s{}.
     .d_fif = 0, .d_chr = 0, .d_dir = 0,
     .d_blk = 0, .d_reg = 0, .d_lnk = 0,
-    .d_sok = 0, .d_wht = 0, .d_unk = 0
+    .d_sok = 0, .d_wht = 0, .d_unk = 0,
+    .fqdp = NULL
 };
-
-/**
- * Initialise a list node to the linked-list for storing directory paths.
- */
-dir_node *createDirNode(dp_name *dir)
-{
-    dir_node *new_ent = (dir_node *)calloc(1,sizeof(dir_node));
-
-    if ( new_ent ) {
-        new_ent->dir = dir;
-        dprint("%s", dir);
-        return new_ent;
-    } else {
-        dprint("error %d", errno);
-        perror("unable to allocate directory entry");
-        return NULL;
-    }
-}
 
 /**
  * Initialise the linked-list for storing directory paths.
  */
-dir_list *createDirList()
+dir_list_s *createDirList()
 {
-    dir_list *dir_path = (dir_list *)calloc(1, sizeof(dir_list));
+    dir_list_s *dir_path = (dir_list_s *)calloc(1, sizeof(dir_list_s));
 
     if ( dir_path ) {
-        dprint("initialised %s", "dir_path");
+        Dprint("initialised %s", "dir_path");
         return dir_path;
     } else {
-        dprint("error %d", errno);
+        Dprint("error %d", errno);
         perror("unable to allocate directory path list");
         return NULL;
     }
 }
 
 /**
- * Fetch the current number of directory entries from the linked-list.
+ * Initialise a list node to the linked-list for storing directory paths.
  */
-int numDirs(dir_list *paths)
+dir_node_s *createDirNode(dp_name *dir)
 {
-    return paths->num_dirs;
+    dir_node_s *new_ent = (dir_node_s *)calloc(1,sizeof(dir_node_s));
+
+    if ( new_ent ) {
+        new_ent->dir = dir;
+        Dprint("%s", dir);
+        return new_ent;
+    } else {
+        Dprint("error %d", errno);
+        perror("unable to allocate directory entry");
+        return NULL;
+    }
+}
+
+/**
+ * Test directory, identified by pointer to const char, prior to further action.
+ */
+bool testDir(char *dir)
+{
+    struct stat sb;
+    char *fc = NULL;
+
+    asprintf(&fc, "%c", dir[0]);
+
+    if ( stat(dir,&sb) == 0 && S_ISDIR(sb.st_mode) ) {
+        if ( strncmp(fc, DFS, 1) == 0 ) {
+            /// Push any legitimate, fully-qualified directory path to a
+            /// holding element to be pushed to linked-list.
+            de.fqdp = dir;
+            Dprint("user: %s", de.fqdp);
+        } else {
+            /// Fully-qualify any legitimate, relative directory path and
+            /// push to holding element.
+            chdir(dir);
+            de.fqdp = getwd((char *)dir);
+            Dprint("FQP: %s", de.fqdp);
+        }
+
+        Dprint("%s", "TRUE");
+        return true;
+    }
+
+    free(fc);
+
+    /// If neither of the above conditions holds, `*dir` is not a valid
+    /// directory path.
+    Dprint("%s","FALSE");
+    return false;
 }
 
 /**
  * Add a directory entry to the linked-list.
  */
-void addDir(dir_list *paths, dir_node *dir_ent)
+void addDir(dir_list_s *paths, dir_node_s *dir_node, char *path_arg)
 {
-    dprint("addDir %s", dir_ent->dir);
-    dir_node *next = paths->head;
-    paths->head = dir_ent;
-    dir_ent->next = next;
+    if ( testDir(path_arg) ) {
+        Dprint("%s", "testDir returned TRUE to addDir");
+        dir_node = createDirNode(de.fqdp);
+        Dprint("addDir %s", dir_node->dir);
+        dir_node_s *next = paths->head;
+        paths->head = dir_node;
+        dir_node->next = next;
 
-    dprint("increment num_dirs from %d", paths->num_dirs);
-    paths->num_dirs++;
-    dprint("increment num_dirs to %d", paths->num_dirs);
+        paths->num_dirs++;
+        Dprint("num_dirs: %d", paths->num_dirs);
+    } else {
+        Dprint("errno: %d", errno);
+        perror(path_arg);
+    }
+}
+
+/**
+ * Add the stats from a node entry (directory path) to the linked-list.
+ */
+void getDirStats(dir_node_s *dir_node)
+{
+    Dprint("%s", dir_node->dir);
+    DIR *dp;
+    dp = opendir(dir_node->dir);
+    struct dirent *ep = readdir(dp); // from sys/dirent.h
+
+    if ( dp ) {
+        // Duff's Device?
+        while ( (ep = readdir(dp)) ) {
+            Dprint("ep = %hhu", ep->d_type);
+            switch(ep->d_type) {
+            case DT_BLK:  ++(de.d_blk);
+                Dprint("DT_BLK: %d", de.d_blk);  break;
+            case DT_CHR:  ++(de.d_chr);
+                Dprint("DT_CHR: %d", de.d_chr);  break;
+            case DT_DIR:  ++(de.d_dir);
+                Dprint("DT_DIR: %d", de.d_dir);  break;
+            case DT_LNK:  ++(de.d_lnk);
+                Dprint("DT_LNK: %d", de.d_lnk);  break;
+            case DT_REG:  ++(de.d_reg);
+                Dprint("DT_REG: %d", de.d_reg);  break;
+            case DT_WHT:  ++(de.d_wht);
+                Dprint("DT_WHT: %d", de.d_wht);  break;
+            case DT_FIFO: ++(de.d_fif);
+                Dprint("DT_FIFO: %d", de.d_fif); break;
+            case DT_SOCK: ++(de.d_sok);
+                Dprint("DT_SOCK: %d", de.d_sok); break;
+            default:      ++(de.d_unk);
+                Dprint("DT_UNK: %d", de.d_unk);  break;
+            }
+        }
+    } else {
+        Dprint("error %d", errno);
+        perror(dir_node->dir);
+    }
+
+    (void)closedir(dp);
 }
 
 /**
  * Get a list of the directory path entries in the linked-list.
  */
-void getPaths(dir_list *paths)//, void (*printDir)(dp_name *dir))
+void getPaths(dir_list_s *paths)
 {
-    dir_node *cursor = paths->head;
+    dir_node_s *cursor = paths->head;
 
-    dprint("list has %d entries", numDirs(paths));
     while ( cursor ) {
-        dprint("cursor is not %s", "NULL");
-        getDir(cursor);//, printDir);
+        getDirStats(cursor);
+        Dprint("%s", cursor->dir);
         cursor = cursor->next;
     }
-}
-
-/**
- * Fetch one node entry (directory path) from the linked-list.
- */
-void getDir(dir_node *dir_ent)//, void (*printDir)(dp_name *dir))
-{
-    dprint("%s", dir_ent->dir);
-    DIR *dp;
-    dp = opendir(dir_ent->dir);
-    struct dirent *ep = readdir(dp);
-
-    if ( dp ) {
-        while ( (ep = readdir(dp)) ) {
-            dprint("ep = %hhu", ep->d_type);
-            switch(ep->d_type) {
-            case DT_BLK:  ++(de.d_blk);
-                dprint("DT_BLK: %d", de.d_blk); break;
-            case DT_CHR:  ++(de.d_chr);
-                dprint("DT_CHR: %d", de.d_chr); break;
-            case DT_DIR:  ++(de.d_dir);
-                dprint("DT_DIR: %d", de.d_dir); break;
-            case DT_LNK:  ++(de.d_lnk);
-                dprint("DT_LNK: %d", de.d_lnk); break;
-            case DT_REG:  ++(de.d_reg);
-                dprint("DT_REG: %d", de.d_reg); break;
-            case DT_WHT:  ++(de.d_wht);
-                dprint("DT_WHT: %d", de.d_wht); break;
-            case DT_FIFO: ++(de.d_fif);
-                dprint("DT_FIFO: %d", de.d_fif); break;
-            case DT_SOCK: ++(de.d_sok);
-                dprint("DT_SOCK: %d", de.d_sok); break;
-            default:      ++(de.d_unk);
-                dprint("DT_UNK: %d", de.d_unk); break;
-            }
-        }
-    } else {
-        dprint("error %d", errno);
-        perror(dir_ent->dir);
-    }
-
-    (void)closedir(dp);
 }
 
 /**
@@ -221,111 +251,22 @@ void getDir(dir_node *dir_ent)//, void (*printDir)(dp_name *dir))
  * strings. Takes an `int` of how many things in question and a pointer to
  * `char` where a letter `s` will be populated or nulled.
  */
-char *ess(int *cnt, char *s)
+char *pl(int *cnt, char *p, enum action act)
 {
-    switch(*(cnt)) {
+    switch( *(cnt) ) {
     case 1:
-        s = "";
+        if ( act == add ) p = "";
+        if ( act == rep ) p = "y";
         break;
     default:
-        s = "s";
+        if ( act == add ) p = "s";
+        if ( act == rep ) p = "ies";
         break;
     }
 
-    return s;
+    return p;
 }
 
-/**
- * Test directory, identified by pointer to const char, prior to further action.
- */
-bool testDir(const char *dir)
-{
-    struct stat sb;
-
-    dprint("testing directory: %s ", dir);
-    if (stat(dir,&sb) == 0 && S_ISDIR(sb.st_mode)) {
-        dprint("%s", "TRUE");
-        return true;
-    }
-
-    dprint("%s","FALSE");
-    return false;
-}
-
-/**
- * Populate the `dir_ent_s` struct with the info from each directory in
- * the `dl_s{}` list.
- *//*
-bool getDirStats(struct dl_s *head)
-{
-    struct dl_s *tmp = head;
-
-    if ( tmp ) {
-        dprint("entered getDirStats(%s)", tmp->next->dir_path);
-        while ( tmp->next ) {
-            if ( tmp->next->dir_path ) {
-                DIR *dp;
-                dp = opendir(tmp->next->dir_path);
-                struct dirent *ep = readdir(dp);
-                dprint("entered getDirStats('%s', '%s', %i)",
-                       tmp->next->dir_path, ep->d_name, opt.rec);
-                if ( dp != NULL ) {
-                    while ( (ep = readdir(dp)) ) {
-                        dprint("ep = %hhu", ep->d_type);
-                        switch(ep->d_type) {
-                        case DT_BLK:  ++(de.d_blk);
-                            dprint("DT_BLK: %d", de.d_blk); break;
-                        case DT_CHR:  ++(de.d_chr);
-                            dprint("DT_CHR: %d", de.d_chr); break;
-                        case DT_DIR:  ++(de.d_dir);
-                            dprint("DT_DIR: %d", de.d_dir); break;
-                        case DT_LNK:  ++(de.d_lnk);
-                            dprint("DT_LNK: %d", de.d_lnk); break;
-                        case DT_REG:  ++(de.d_reg);
-                            dprint("DT_REG: %d", de.d_reg); break;
-                        case DT_WHT:  ++(de.d_wht);
-                            dprint("DT_WHT: %d", de.d_wht); break;
-                        case DT_FIFO: ++(de.d_fif);
-                            dprint("DT_FIFO: %d", de.d_fif); break;
-                        case DT_SOCK: ++(de.d_sok);
-                            dprint("DT_SOCK: %d", de.d_sok); break;
-                        default:      ++(de.d_unk);
-                            dprint("DT_UNK: %d", de.d_unk); break;
-                        }
-   *//*
-                        if (ep->d_type == DT_DIR && opt.rec == true &&
-                            strncmp(ep->d_name, CD, ep->d_namlen) != 0 &&
-                            strncmp(ep->d_name, PD, ep->d_namlen) != 0 ) {
-                            dprint("strncmp(CD): %d, %s, %s, %d",
-                                   strncmp(ep->d_name, CD, ep->d_namlen),
-                                   ep->d_name, CD, ep->d_namlen);
-                            dprint("strncmp(PD): %d, %s, %s, %d",
-                                   strncmp(ep->d_name, PD, ep->d_namlen),
-                                   ep->d_name, PD, ep->d_namlen);
-                            dprint("rec: de.dn[0]: %s, ep->d_name: %s",
-                                   de.dn[0], ep->d_name);
-                            dprint("calling: getDirStats(%s, %s, %i)",
-                                   ep->d_name, de.dn[0], opt.rec);
-                        }
-   *//*
-                    }
-                } else {
-                    perror(tmp->next->dir_path);
-                }
-
-                (void)closedir(dp);
-            }
-        }
-
-        tmp = tmp->next->next;
-    } else {
-        perror("unable to access directory list");
-        return false;
-    }
-
-    return true;
-}
-*/
 /**
  * Takes the number of input directories, a pointer to the list of
  * directories, and the directory entry statistics structure and
@@ -333,20 +274,21 @@ bool getDirStats(struct dl_s *head)
  */
 void collateOutput()
 {
-    char *s = malloc(sizeof(char) + 1);
+    char *c = malloc(sizeof(char));
 
     printf("Totals:\n");
-    printf("%8d:directorie%s\n",             de.d_dir, ess(&de.d_dir, s));
-    printf("%8d:FIFO file%s\n",              de.d_fif, ess(&de.d_fif, s));
-    printf("%8d:character special file%s\n", de.d_chr, ess(&de.d_chr, s));
-    printf("%8d:block special file%s\n",     de.d_blk, ess(&de.d_blk, s));
-    printf("%8d:regular file%s\n",           de.d_reg, ess(&de.d_reg, s));
-    printf("%8d:symlink%s\n",                de.d_lnk, ess(&de.d_lnk, s));
-    printf("%8d:socket%s\n",                 de.d_sok, ess(&de.d_sok, s));
-    printf("%8d:union whiteout file%s\n",    de.d_wht, ess(&de.d_wht, s));
-    printf("%8d:unknown file type%s\n",      de.d_unk, ess(&de.d_unk, s));
+    printf("%8d:director%s\n",               de.d_dir, pl(&de.d_dir, c, rep));
+    printf("%8d:FIFO file%s\n",              de.d_fif, pl(&de.d_fif, c, add));
+    printf("%8d:character special file%s\n", de.d_chr, pl(&de.d_chr, c, add));
+    printf("%8d:block special file%s\n",     de.d_blk, pl(&de.d_blk, c, add));
+    printf("%8d:regular file%s\n",           de.d_reg, pl(&de.d_reg, c, add));
+    printf("%8d:symlink%s\n",                de.d_lnk, pl(&de.d_lnk, c, add));
+    printf("%8d:socket%s\n",                 de.d_sok, pl(&de.d_sok, c, add));
+    printf("%8d:union whiteout file%s\n",    de.d_wht, pl(&de.d_wht, c, add));
+    printf("%8d:unknown file type%s\n",      de.d_unk, pl(&de.d_unk, c, add));
 
-    //displayOutput();
+    free(c);
+    displayOutput();
 }
 
 /**
@@ -354,7 +296,6 @@ void collateOutput()
  */
 int displayOutput()
 {
-    dprint("%i",opt.rec);
     return 0;
 }
 
@@ -363,12 +304,6 @@ int displayOutput()
  */
 int main(int argc, char *argv[])
 {
-    /// Initialise the variables and linked list for storing directory paths.
-    char *this_dir = malloc(MAXPATHLEN);
-    dp_name *dir_path = NULL;
-    dir_node *dir_ent = NULL;
-    dir_list *dir_list = createDirList();
-
     /// Initialise, read, and set the various user options.
     int param_index = 0;
     cag_option_context context;
@@ -377,9 +312,6 @@ int main(int argc, char *argv[])
 
     while (cag_option_fetch(&context)) {
         switch (cag_option_get_identifier(&context)) {
-        case 'r':
-            opt.rec = true;
-            break;
         case 'h':
             printf("Usage: dstat [OPTION]... DIRECTORY...\n");
             printf("Quickly gathers and reports the numbers of various file ");
@@ -401,35 +333,27 @@ int main(int argc, char *argv[])
         }
     }
 
+    /// Initialise the variables and linked list for storing directory paths.
+    dp_name *dir_path = NULL;
+    dir_node_s *dir_node = NULL;
+    dir_list_s *dir_list = createDirList();
     int dir_cnt = 0;
 
-    for ( param_index = cag_option_get_index(&context); param_index < argc;
-         ++param_index ) {
-        dprint("loop: %02d: param_index = %d, dir_cnt = %d",
+    /// Loop over all non-option arguments (directory paths or junk data)
+    /// and add valid paths to the linked-list.
+    for ( param_index = cag_option_get_index(&context) ;
+          param_index < argc ; ++param_index ) {
+        Dprint("loop: %02d: param_index = %d, dir_cnt = %d",
                dir_cnt, param_index, dir_cnt);
-        if ( testDir(argv[param_index]) ) {
-            this_dir = argv[param_index];
-            dir_ent = createDirNode(this_dir);
-            addDir(dir_list, dir_ent);
-            ++dir_cnt;
-        } else {
-            dprint("error %d", errno);
-            perror(argv[param_index]);
-            return EXIT_FAILURE;
-        }
+        addDir(dir_list, dir_node, argv[param_index]);
+        ++dir_cnt;
     }
 
-    dprint("dir_cnt: %d", dir_cnt);
+    /// If no directory paths were supplied from the command line,
+    /// add the current working directory to the linked-list.
+    Dprint("dir_cnt: %d", dir_cnt);
     if ( dir_cnt == 0 ) {
-        this_dir = getcwd(dir_path, MAXPATHLEN);
-        if ( testDir(this_dir) ) {
-            dir_ent = createDirNode(this_dir);
-            dir_cnt = 1;
-            addDir(dir_list, dir_ent);
-        } else {
-            dprint("error %d", errno);
-            perror("unable to add current working directory");
-        }
+        addDir(dir_list, dir_node, getcwd(dir_path, MAXPATHLEN));
     }
 
     getPaths(dir_list);
